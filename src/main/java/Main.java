94f9f45c71d217213c8dc18e24de9d06d635ce45
Emasher <evan@spearman.mb.ca>
2014-05-13 15:41:37 -0500

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import org.eclipse.swt.widgets.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Create a blank SWT window
+
+        Display display = new Display();
+        Shell shell = new Shell(display);
+
+        shell.open();
+
+        while(! shell.isDisposed()) {
+            if(! display.readAndDispatch()) {
+                display.sleep();
+            }
+        }
+
+        display.dispose();
+    }
+
+}
