3607504dbd26ef1df06aede6c8ceaeb0f57bc541
billal007 <billalkohistani@gmail.com>
2014-06-09 11:20:43 -0700

    Created unit tests for RegisterStudentsTest.java.
    Signed-off-by: billal007 <billalkohistani@gmail.com>

---
+++ src/test/business/logic/RegisterStudentsTest.java
@@ -0,0 +1,35 @@
+package test.business.logic;
+
+import junit.framework.TestCase;
+import lms.business.Student;
+import lms.business.logic.RegisterStudent;
+import java.util.UUID;
+
+public class RegisterStudentsTest extends TestCase
+{
+	UUID id = UUID.randomUUID();
+	UUID term = UUID.randomUUID();
+	
+	Student newStudent;
+	Student testUpdate; 
+	Student testInsert;
+	Student empty; 
+
+	public void setUp() throws Exception 
+	{
+		newStudent = new Student(id, "billal", "kohistani", "umkohisb", 1234567, true, term);
+		newStudent.save();
+	}
+	public void testUpdateStudent()
+	{
+		testUpdate = RegisterStudent.upsertStudent(newStudent, "Morgan", "kohistani","umkohisb", 1234567, true, term);
+		assertTrue("updateStudent() did not update student number", newStudent.getFirstName() == "Morgan");
+	}
+	public void testInsertStudent()
+	{
+		empty = null;
+		testInsert = RegisterStudent.upsertStudent(empty, "billal", "prat", "jbrco", 0000000 , false, term);
+		assertNotNull("insertStudent() did not add new student and is null", testInsert);	
+	}
+}
+	
