b27ce9b984ab14bd0621982fa532d08ffeb7782f
taylerfrederick <taylerfrederick@gmail.com>
2014-06-09 07:37:47 -0500

    Changed formatting

--- src/lms/business/logic/CurrentTermInfo.java
+++ src/lms/business/logic/CurrentTermInfo.java
@@ -7,17 +7,23 @@ public class CurrentTermInfo
 	private static UUID id;
 	private static String name;
 	
-	public static UUID getId() {
+	public static UUID getId() 
+	{
 		return id;
 	}
-	public static void setId(UUID id) {
+	
+	public static void setId(UUID id) 
+	{
 		CurrentTermInfo.id = id;
 	}
-	public static String getName() {
+	
+	public static String getName() 
+	{
 		return name;
 	}
-	public static void setName(String name) {
-		CurrentTermInfo.name = name;
-	}
 	
+	public static void setName(String name) 
+	{
+		CurrentTermInfo.name = name;
+	}	
 }
