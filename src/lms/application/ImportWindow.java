a780c63b99ed94b5584a14342a7a8978181ac299
taylerfrederick <taylerfrederick@gmail.com>
2014-06-09 05:09:50 -0500

    Added call to SpreadsheetImporter

--- src/lms/application/ImportWindow.java
+++ src/lms/application/ImportWindow.java
@@ -2,6 +2,8 @@ package lms.application;
 
 import java.io.File;
 
+import lms.business.logic.SpreadsheetImporter;
+
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.MessageBox;
@@ -63,6 +65,8 @@ public class ImportWindow
 				File f = new File(filePath);
 				if(f.isFile())
 				{
+					SpreadsheetImporter.importStudents(filePath);
+					
 					MessageBox dlgSuccess = new MessageBox(shell, SWT.OK);
 					dlgSuccess.setText("Completed");
 					dlgSuccess.setMessage("Importing completed");
