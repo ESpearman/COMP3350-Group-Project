88e18d8869c217556615bfcbb67719538cbdfeb0
ESpearman <evanspearman@gmail.com>
2014-06-09 15:28:06 -0500

    Added build scripts

---
+++ Compile.bat
@@ -0,0 +1,14 @@
+cd bin
+erase /S /Q *.class
+cd ..
+
+set CLASSPATH=.;bin\;lib\*;src\test\testFiles\*
+
+
+javac -d bin\ -cp %classpath% src\lms\application\*.java src\lms\business\*.java src\lms\business\logic\*.java src\lms\db\*.java src\lms\stubdb\*.java
+
+javac -d bin\ -cp %classpath% src\test\business\logic\*.java
+javac -d bin\ -cp %classpath% src\test\stubdb\*.java
+javac -d bin\ -cp %classpath% src\test\*.java
+
+pause
---
+++ src/test/AllTests.java
@@ -0,0 +1,33 @@
+package test;
+
+import test.business.logic.RegisterStudentsTest;
+import test.business.logic.SearchStudentTest;
+import test.business.logic.SpreadsheetImporterTest;
+import test.stubdb.StubDBTest;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests
+{
+	public static TestSuite suite;
+
+    public static Test suite()
+    {
+        suite = new TestSuite("All tests");
+        testLogic();
+        testStubDB();
+        return suite;
+    }
+
+    private static void testLogic()
+    {
+        suite.addTestSuite(RegisterStudentsTest.class);
+        suite.addTestSuite(SearchStudentTest.class);
+        suite.addTestSuite(SpreadsheetImporterTest.class);
+    }
+
+    private static void testStubDB()
+    {
+        suite.addTestSuite(StubDBTest.class);
+    }
+}
