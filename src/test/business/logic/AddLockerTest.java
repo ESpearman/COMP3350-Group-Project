9fd746158482b141e102d9169a7e3f6b69a00386
billal007 <billalkohistani@gmail.com>
2014-06-27 08:18:09 -0700

    Created AddLockerTest.java
    Signed-off-by: billal007 <billalkohistani@gmail.com>

---
+++ src/test/business/logic/AddLockerTest.java
@@ -0,0 +1,36 @@
+package test.business.logic;
+
+import java.util.UUID;
+
+import junit.framework.TestCase;
+import lms.business.Locker;
+import lms.business.LockerSize;
+import lms.persistence.DBInjector;
+import lms.persistence.DBProxy;
+import lms.business.logic.AddLocker;
+
+public class AddLockerTest extends TestCase
+{
+	UUID id = UUID.randomUUID();
+	UUID term = UUID.randomUUID();
+	UUID building = UUID.randomUUID();
+	
+	LockerSize size = LockerSize.FULL; 
+	Locker newLocker;
+	Locker testInsert;
+
+	
+	public void setUp() throws Exception
+	{
+		DBProxy.instance = new DBProxy();
+		DBInjector.injectInto(DBProxy.instance, true);
+		newLocker = new Locker(id, term, 007, building, size);
+		newLocker.save();
+	}
+	public void testInsert()
+	{
+		testInsert = AddLocker.insert(term, 001, building, size);
+		assertNotNull("insertLocker() did not add new locker, locker is null", testInsert);
+	}
+
+}
