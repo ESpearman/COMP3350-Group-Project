7dc22ddfc154eded422778db8c9ff4084ea70f7f
ESpearman <evanspearman@gmail.com>
2014-07-14 14:09:53 -0500

    Fixed demo data generation bug

--- src/lms/reset/DBReset.java
+++ src/lms/reset/DBReset.java
@@ -3,8 +3,12 @@ package lms.reset;
 import java.util.UUID;
 
 import lms.businesslogic.CurrentTermInfo;
-import lms.businesslogic.DemoDataGenerator;
 import lms.config.ConfigData;
+import lms.domainobjects.Building;
+import lms.domainobjects.Locker;
+import lms.domainobjects.LockerSize;
+import lms.domainobjects.Rental;
+import lms.domainobjects.Student;
 import lms.domainobjects.Term;
 import lms.persistence.ConnectionPool;
 import lms.persistence.DBInjector;
@@ -27,7 +31,26 @@ public class DBReset
 		CurrentTermInfo.currentTerm = term;
 		term.save();
 		
-		DemoDataGenerator.generate();
+		Building building1 = new Building("Machray Hall");
+		building1.save();
+		Building building2 = new Building("Armes");
+		building2.save();
+		
+		Locker locker1 = new Locker(CurrentTermInfo.currentTerm.getId(), 1, building1.getId(), LockerSize.FULL);
+		locker1.save();
+		
+		Locker locker2 = new Locker(CurrentTermInfo.currentTerm.getId(), 2, building2.getId(), LockerSize.FULL);
+		locker2.save();
+		
+		Student student1 = new Student("Some", "Dude", "1@1.com", 1000, true, CurrentTermInfo.currentTerm.getId());
+		student1.save();
+		
+		Rental rental1 = new Rental(CurrentTermInfo.currentTerm.getId(), student1.getId(), locker1.getId(), 0.0, true);
+		rental1.save();
+		
+		System.out.println("Completed Generating Demo Data");
+		
+		ConnectionPool.clearConnections();
 		
 		System.out.println("DB reset complete");
 	}
